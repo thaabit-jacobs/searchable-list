bcd9b4182d4e154f701e3e979825793aaa9ed86e
thaabit <mohammedthaabitjacobs@gmail.com>
2022-02-21 21:17:39 +0200

    search

--- src/index.js
+++ src/index.js
@@ -4,14 +4,57 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let items = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
+
+function filterBy(category){
+  return items
+        .filter(item => item.category === category); 
+}
+
+function Table(props){
+  return (
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Contact</th>
+      </tr>
+
+      <tr>Sporting Goods</tr>
+      <TableRowsData items={props.items} category="Sporting Goods"/>
+
+      <tr>Electronics</tr>
+      <TableRowsData items={props.items} category="Electronics"/>
+    </table>
+  );
+}
+
+
+
+function TableRowsData(props){
+  return (
+    props.items
+    .filter(item => item.category === props.category)
+    .map(item => {
+        return (
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+          </tr>
+        )
+    })
+  )
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Table items={items}/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
