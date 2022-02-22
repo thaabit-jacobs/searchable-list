1c9f51c98902264eecf66555dc35097ea2255fec
thaabit <mohammedthaabitjacobs@gmail.com>
2022-02-22 16:56:05 +0200

    starting compsoing

--- src/index.js
+++ src/index.js
@@ -4,18 +4,50 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-let items = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
+class SearchForm extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = 
+    {
+      items: [
+        {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+        {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+        {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+        {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+        {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+        {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+      ]
+    };
 
-function filterBy(category){
-  return items
-        .filter(item => item.category === category); 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    let searchTerm = event.target.value;
+
+    this.setState(
+        prevSate => ({
+          items: prevSate.filter(item => item.name.indexOf(searchTerm) !== -1)
+        })
+    );
+
+  }
+
+  render() {
+    return (
+      <div>
+        <fieldset>
+          <label>
+            Name:
+            <input type="text" onChange={this.handleChange} />
+          </label>
+        </fieldset>
+
+
+      <Table items={this.state.items}/>
+      </div>
+    );
+  }
 }
 
 function Table(props){
@@ -26,10 +58,15 @@ function Table(props){
         <th>Contact</th>
       </tr>
 
-      <tr>Sporting Goods</tr>
+    
+      <tr>
+        <td>Sporting Goods</td>
+      </tr>
       <TableRowsData items={props.items} category="Sporting Goods"/>
 
-      <tr>Electronics</tr>
+      <tr>
+        <td>Electronics</td>
+      </tr>
       <TableRowsData items={props.items} category="Electronics"/>
     </table>
   );
@@ -53,7 +90,7 @@ function TableRowsData(props){
 }
 
 ReactDOM.render(
-  <Table items={items}/>,
+  <SearchForm />,
   document.getElementById('root')
 );
 
